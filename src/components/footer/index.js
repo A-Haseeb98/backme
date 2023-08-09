6f799e706b8d2869d3891861db2cbdc28a18d722
a-haseeb98 <haseeb.dar.dev@gmail.com>
2023-08-09 13:36:05 +0500

    second

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,31 +3,33 @@ import img from "../../assets/images/footer_image.svg"
 
 export default function Footer() {
     return (
-        <div>
-            <div>
-                <div>
-                    <h1>BackMe is member of:</h1>
-                    <img src={img} alt="" />
+        <div className="footer_bg">
+            <div className="footer_container">
+                <div className="footer_wrapper">
+                    <div className="footer_left">
+                        <h1>BackMe is member of:</h1>
+                        <span><img src={img} alt="" /></span>
+                    </div>
+                    <div className="footer_middle">
+                        <h1>Contacts</h1>
+                        <ul>
+                            <li>Contact us</li>
+                            <li>Cancel subscription</li>
+                            <li>Report abuse</li>
+                        </ul>
+                    </div>
+                    <div className="footer_right">
+                        <h1>Company</h1>
+                        <ul>
+                            <li>About</li>
+                            <li>How does it work?</li>
+                            <li>Your privacy matters</li>
+                            <li>Terms and conditions</li>
+                        </ul>
+                    </div>
                 </div>
-                <div>
-                    <h1>Contacts</h1>
-                    <ul>
-                        <li>Contact us</li>
-                        <li>Cancel subscription</li>
-                        <li>Report abuse</li>
-                    </ul>
-                </div>
-                <div>
-                    <h1>Company</h1>
-                    <ul>
-                        <li>About</li>
-                        <li>How does it work?</li>
-                        <li>Your privacy matters</li>
-                        <li>Terms and conditions</li>
-                    </ul>
-                </div>
+                <p>© 2023 All rights reserved - Nine Nine B.V. The Hague, The Netherlands - Powered by Oxjno</p>
             </div>
-            <p>© 2023 All rights reserved - Nine Nine B.V. The Hague, The Netherlands - Powered by Oxjno</p>
         </div>
     )
 }
--- src/components/navbaar/index.js
+++ src/components/navbaar/index.js
@@ -5,19 +5,21 @@ import img from "../../assets/images/arrow.svg"
 
 export default function Navbar() {
     return (
-        <div className="navbar_container">
-            <div className="logo_container">
-                <img src={logo} alt="" />
-            </div>
-            <div className="navbar_list_items">
-                <ul>
-                    <li>Home</li>
-                    <li>Blog</li>
-                    <li>Dashboard</li>
-                    <li>Log in</li>
-                    <li className="menu_button"><button>Join BackMe</button></li>
-                    <li>ENG<span><img src={img} alt="" /></span></li>
-                </ul>
+        <div className="navbar_container_bg">
+            <div className="navbar_container">
+                <div className="logo_container">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="navbar_list_items">
+                    <ul>
+                        <li>Home</li>
+                        <li>Blog</li>
+                        <li>Dashboard</li>
+                        <li>Log in</li>
+                        <li className="menu_button"><button>Join BackMe</button></li>
+                        <li>ENG<span><img src={img} alt="" /></span></li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
